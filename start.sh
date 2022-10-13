fe92e87cf588d23068698c3584ad73b193ef4206
Bryan Finster <bryan.finster@gmail.com>
2022-10-13 09:08:54 -0500

    Update start

--- start.sh
+++ start.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-email_prompt() {
-  read -p "Please provide us with your email address: " EMAIL
-  while true; do
-    if [ -z "$EMAIL" ]
-    then
-      break
-    fi
-    read -p "Is this email correct? $EMAIL - [y/n]: " yn
-    case $yn in
-        [Yy]*) break  ;;
-        [Nn]*) email_prompt; exit 1 ;;
-    esac
-  done
-  printf "Thank you! 🙏\n"
-}
-
-EMAIL_FILE=".faros-email"
-if [[ -f "$EMAIL_FILE" ]]; then
-    EMAIL=$(cat $EMAIL_FILE)
-else
-    printf "Hello 👋 Welcome to Faros Community Edition! 🤗\n\n"
-    printf "Want to stay up to date with the latest community news? (we won't spam you)\n"
-    email_prompt
-    echo "$EMAIL" > $EMAIL_FILE
-fi
-
-export FAROS_EMAIL=$EMAIL
+export FAROS_EMAIL=none@none.com
 
 # Ensure we're using the latest faros-init image
 export FAROS_INIT_IMAGE=farosai.docker.scarf.sh/farosai/faros-ce-init:latest
@@ -36,8 +10,8 @@ docker-compose pull faros-init
 if [[ $(uname -m 2> /dev/null) == 'arm64' ]]; then
     # Use Metabase images built for Apple M1
     METABASE_IMAGE="farosai.docker.scarf.sh/farosai/metabase-m1" \
-    docker-compose up --build --remove-orphans
+    docker-compose up --build --remove-orphans &
 else
-    docker-compose up --build --remove-orphans
+    docker-compose up --build --remove-orphans &
 fi
 
